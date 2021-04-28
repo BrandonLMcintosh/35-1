1c2eb5330dd2c555ec7be35c1f407748d0f91d65
Brandon McIntosh <brandon.l.mcintosh@gmail.com>
2021-04-28 09:48:52 -0700

    initial commit

---
+++ routes/companies.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const db = require("../db");
+const router = new express.Router();
+
+router.get("", async () => {});
+
+router.post("", async () => {});
+
+router.get("/:code", async () => {});
+
+router.put("/:code", async () => {});
+
+router.delete("/:code", async () => {});
+
+module.exports = router;
---
+++ routes/invoices.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const db = require("../db");
+const router = new express.Router();
+
+router.get("", async () => {});
+
+router.post("", async () => {});
+
+router.get("/:id", async () => {});
+
+router.put("/:id", async () => {});
+
+router.delete("/:id", async () => {});
+
+module.exports = router;
