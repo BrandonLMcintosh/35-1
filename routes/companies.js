5160989b554504984248b32e55bb1798b48e0366
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2021-04-28 22:09:16 -0700

    finished

--- routes/companies.js
+++ routes/companies.js
@@ -2,14 +2,84 @@ const express = require("express");
 const db = require("../db");
 const router = new express.Router();
 
-router.get("", async () => {});
+router.get("", async (req, res, next) => {
+	try {
+		const result = await db.query("SELECT * FROM companies");
+		return res.json({
+			companies: result.rows,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
 
-router.post("", async () => {});
+router.post("", async (req, res, next) => {
+	try {
+		const { code, name, description } = req.body;
+		const result = await db.query(
+			`INSERT INTO companies
+            (code, name, description)
+            VALUES
+            ($1,$2,$3)
+            RETURNING code, name, description`,
+			[code, name, description]
+		);
+		res.json({
+			added_company: result.rows,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
 
-router.get("/:code", async () => {});
+router.get("/:code", async (req, res, next) => {
+	try {
+		const code = req.params.code;
+		const result = await db.query(
+			`SELECT * FROM companies
+            WHERE code=$1`,
+			[code]
+		);
+		return res.json({
+			company: result.rows,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
 
-router.put("/:code", async () => {});
+router.put("/:code", async (req, res, next) => {
+	try {
+		const code = req.params.code;
+		const { name, description } = req.body;
+		const result = await db.query(
+			`UPDATE companies SET name=$1, description=$2
+            WHERE code=$3
+            RETURNING code, name, description`,
+			[name, description, code]
+		);
+		return res.json({
+			updated_company: result.rows,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
 
-router.delete("/:code", async () => {});
+router.delete("/:code", async (req, res, next) => {
+	try {
+		const code = req.params.code;
+		await db.query(
+			`DELETE FROM users
+            WHERE code=$1`,
+			[code]
+		);
+		return res.json({
+			deleted_company: code,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
 
 module.exports = router;
--- routes/invoices.js
+++ routes/invoices.js
@@ -6,7 +6,7 @@ router.get("", async (req, res, next) => {
 	try {
 		const result = await db.query(`SELECT * FROM invoices`);
 		return res.json({
-			invoices: result,
+			invoices: result.rows,
 		});
 	} catch (err) {
 		next(err);
@@ -14,21 +14,72 @@ router.get("", async (req, res, next) => {
 });
 
 router.post("", async (req, res, next) => {
-	try{
-		const {id, comp_code, amt}
+	try {
+		const { id, comp_code, amt, add_date } = req.body;
 		const result = await db.query(
 			`INSERT INTO invoices 
 			(id, comp_code, amt, paid, add_date) 
-			VALUES ($1, $2, $3, $4, $5)`, [])
-	}catch(err){
+			VALUES ($1, $2, $3, $4, $5)
+			RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+			[id, comp_code, amt, false, add_date]
+		);
+		res.json({
+			added_invoice: result.rows,
+		});
+	} catch (err) {
 		next(err);
 	}
 });
 
-router.get("/:id", async (req, res, next) => {});
+router.get("/:id", async (req, res, next) => {
+	try {
+		const id = req.params.id;
+		const result = await db.query(
+			`SELECT * FROM invoices
+			WHERE id=$1`,
+			[id]
+		);
+		return res.json({
+			invoice: result.rows,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
 
-router.put("/:id", async (req, res, next) => {});
+router.patch("/:id", async (req, res, next) => {
+	try {
+		const id = req.params.id;
+		const { comp_code, amt, paid, paid_date } = req.body;
+		const result = await db.query(
+			`UPDATE invoices
+			SET comp_code=$1, amt=$2, paid=$3, paid_date=$4
+			WHERE id=$5
+			RETURNING id, comp_code, amt, add_date, paid, paid_date`,
+			[comp_code, amt, paid, paid_date, id]
+		);
+		return res.json({
+			updated_invoice: result.rows,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
 
-router.delete("/:id", async (req, res, next) => {});
+router.delete("/:id", async (req, res, next) => {
+	try {
+		const id = req.params.id;
+		const result = await db.query(
+			`DELETE FROM invoices
+			WHERE id=$1`,
+			[id]
+		);
+		return res.json({
+			deleted_invoice: id,
+		});
+	} catch (err) {
+		next(err);
+	}
+});
 
 module.exports = router;
