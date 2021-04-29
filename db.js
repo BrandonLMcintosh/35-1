7744bf684a13fcdfecb3939f997c98df948b2258
brandon mcintosh <brandon.l.mcintosh@gmail.com>
2021-04-28 18:42:14 -0700

    updates

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { Client } = require("pg");
-const DB_URI = process.env.NODE_ENV === "test" ? `postgresql:///biztime_test` : `postgresql:///biztime`;
+const DB_URI = process.env.NODE_ENV === "test" ? `postgresql://mack:mack@localhost:5432/biztime_test` : `postgresql://mack:mack@localhost:5432/biztime`;
 
 const db = new Client({
 	connectionString: DB_URI,
--- routes/invoices.js
+++ routes/invoices.js
@@ -2,10 +2,10 @@ const express = require("express");
 const db = require("../db");
 const router = new express.Router();
 
-router.get("", async () => {
+router.get("", async (req, res, next) => {
 	try {
 		const result = await db.query(`SELECT * FROM invoices`);
-		return result.json({
+		return res.json({
 			invoices: result,
 		});
 	} catch (err) {
@@ -13,12 +13,22 @@ router.get("", async () => {
 	}
 });
 
-router.post("", async () => {});
+router.post("", async (req, res, next) => {
+	try{
+		const {id, comp_code, amt}
+		const result = await db.query(
+			`INSERT INTO invoices 
+			(id, comp_code, amt, paid, add_date) 
+			VALUES ($1, $2, $3, $4, $5)`, [])
+	}catch(err){
+		next(err);
+	}
+});
 
-router.get("/:id", async () => {});
+router.get("/:id", async (req, res, next) => {});
 
-router.put("/:id", async () => {});
+router.put("/:id", async (req, res, next) => {});
 
-router.delete("/:id", async () => {});
+router.delete("/:id", async (req, res, next) => {});
 
 module.exports = router;
